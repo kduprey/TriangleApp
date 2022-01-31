8d01a479ab874825ebb1f0dff4cea695c6eac0a0
Kenton Duprey <kentonduprey@icloud.com>
2022-01-31 12:38:39 +0000

    adding main view and calculate method

--- Triangle/ViewController.swift
+++ Triangle/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var textBase: UITextField!
+	@IBOutlet weak var textHeight: UITextField!
+	@IBOutlet weak var area: UILabel!
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+	}
+	
+	@IBAction func calculate(_ sender: Any) {
+		
+		let base = (textBase.text! as NSString).floatValue
+		let height = (textHeight.text! as NSString).floatValue
+		
+		area.text = String(format: "%.02f", calculateArea(base: base, height: height))
+		}
+	
+	
+	func calculateArea(base: Float, height: Float) -> Float {
+		let area = 0.5 * base * height
+		return area
 	}
 
 
